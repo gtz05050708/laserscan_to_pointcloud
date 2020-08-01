f1f5af1100e5ff742b060ffadc60e5be166283d0
Qingyuan Zhang <q6zhang@ucsd.edu>
2020-07-31 18:02:54 -0700

    save feature + servo control

--- src/to_point.cpp
+++ src/to_point.cpp
@@ -4,57 +4,89 @@
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Point32.h"
 #include "sensor_msgs/ChannelFloat32.h"
+#include "std_msgs/String.h"
+
+#include "std_msgs/Float32.h"
+#include "std_msgs/UInt16.h"
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
 
 ros::Publisher scan3d_pub;
+ros::Publisher angle_pub;
 
+const int INITIAL_ANGLE = 60;
+const int MAX_ANGLE = 160;
+int servo_angle = INITIAL_ANGLE;
 
+void saveCloud(sensor_msgs::PointCloud &pointcloud) {
+    pcl::PointCloud<pcl::PointXYZI> cloud;
+    cloud.height = 1;
+    cloud.width = pointcloud.points.size();
+    cloud.is_dense = false;
+    cloud.points.resize(cloud.width * cloud.height);
+    for (size_t i = 0; i < cloud.points.size(); i++) {
+        cloud.points[i].x = pointcloud.points[i].x;
+        cloud.points[i].y = pointcloud.points[i].y;
+        cloud.points[i].z = pointcloud.points[i].z;
+        cloud.points[i].intensity = pointcloud.channels[0].values[i];
+
+    }
+    pcl::io::savePCDFileBinary("/home/david/catkin_ws/src/scan_to_point/src/points.pcd", cloud);
+}
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
+
+    //static bool angle_change = true;
     sensor_msgs::LaserScan scan = (sensor_msgs::LaserScan)*msg;
    
-    static uint32_t seq = 0;
-    static sensor_msgs::PointCloud scan3dMsg;
-    scan3dMsg.header.seq = seq++;
-    scan3dMsg.header.stamp = ros::Time::now();
-    scan3dMsg.header.frame_id = "scan_3d";
+    static int seq = 0;
+    static sensor_msgs::PointCloud pointcloud;
+    static std_msgs::String angle_msg;
+    pointcloud.header.seq = seq++;
+    pointcloud.header.stamp = ros::Time::now();
+    pointcloud.header.frame_id = "cloudframe";
 
     static sensor_msgs::ChannelFloat32 channelIntensity; 
     channelIntensity.name = "intensity";
     
-    uint16_t pointCount = 0;
-    static float iy = 0;      
+    uint16_t pointCount = 0;     
+    float phi = 180 - servo_angle +30;
+    phi = phi / 180 * M_PI;
     
-    /*printf( "min angle %6.4lf ", scan.angle_min );
-    printf( "max angle %6.4lf ", scan.angle_max );
-    printf( "increment %6.4lf ", scan.angle_increment );*/
+    if (servo_angle > MAX_ANGLE) {
+        angle_msg.data = std::to_string(INITIAL_ANGLE);
+        angle_pub.publish(angle_msg);
+        saveCloud(pointcloud);
+        ros::shutdown();
+    }
+    
     for (float theta = scan.angle_min; theta < scan.angle_max; theta += scan.angle_increment)
     {
         geometry_msgs::Point32 point;
         float dis = scan.ranges[pointCount];
 
-        /*point.x = dis * cos(theta) * cos(iy);
-        point.y = dis * sin(theta); 
-        point.z = dis * cos(theta) * sin(iy);*/
         point.x = dis * cos(theta);
-        point.y = dis * sin(theta) * sin(iy); 
-        point.z = dis * sin(theta) * cos(iy);
-        scan3dMsg.points.push_back(point);
-        channelIntensity.values.push_back(scan.intensities[pointCount]);      
+        point.y = dis * sin(theta) * sin(phi); 
+        point.z = dis * sin(theta) * cos(phi);
+        pointcloud.points.push_back(point);
+        channelIntensity.values.push_back(scan.intensities[pointCount]);    
         pointCount++;  
     }
     
+    //angle_change = !angle_change;
+    //if (angle_change) {
+    servo_angle++;
+    //}
+    angle_msg.data = std::to_string(servo_angle);
+    angle_pub.publish(angle_msg);
 
-    iy += (M_PI/180);
-    if(iy > M_PI)
-    {
-        return;
-    }
-    scan3dMsg.channels.clear();
-    scan3dMsg.channels.push_back(channelIntensity);
-    scan3d_pub.publish(scan3dMsg);
+    pointcloud.channels.clear();
+    pointcloud.channels.push_back(channelIntensity);
+    scan3d_pub.publish(pointcloud);
 }
+
 
 
 int main (int argc, char **argv)
@@ -62,15 +94,11 @@ int main (int argc, char **argv)
     ros::init(argc, argv, "lidar_transformer");
     ros::NodeHandle n;
     ros::Subscriber scanSubscriber = n.subscribe("/scan", 1000, scanCallback);
-    
     scan3d_pub = n.advertise<sensor_msgs::PointCloud>("scan3d", 1000);
-
-    
-
+    angle_pub = n.advertise<std_msgs::String>("angle", 1000);
     ros::Rate loop_rate(10);
     while (ros::ok())
     {
-
         ros::spinOnce();
         loop_rate.sleep();
     }
